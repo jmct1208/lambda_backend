59d9075a9091afc827cb8d9cf57440fe26c6280e
David Saules <davidsaules@ciencias.unam.mx>
2020-11-26 13:19:13 -0600

    agregué relaciones que faltaban en los modelos

--- taekwondo/src/main/java/com/taekwondo/model/Evento.java
+++ taekwondo/src/main/java/com/taekwondo/model/Evento.java
@@ -1,6 +1,7 @@
 package com.taekwondo.model;
 
 import java.sql.Date;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PositiveOrZero;
@@ -44,6 +47,10 @@ public class Evento {
 	@NotBlank
 	@Column(name = "enlace_facebook")
 	private String enlaceFacebook;
+	
+	@ManyToOne
+	@JoinColumn(name = "tipo_evento_id",nullable=false)
+	private TipoEvento tipoEvento;
 	
 	@ManyToMany
 	@JoinTable(
